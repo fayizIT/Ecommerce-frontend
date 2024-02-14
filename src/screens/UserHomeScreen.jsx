de04ddee57f3d8ae519f7905a39933ed60be33a0
Fayiz <fayizcj94.com>
2024-02-14 14:03:48 +0530

    add the Homescreen

---
+++ src/screens/UserHomeScreen.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import Products from '../component/Products'
+import CarouselComponent from '../component/CarouselComponent'
+
+const UserHomeScreen = () => {
+  return (
+    <>
+    <CarouselComponent />
+    <Products />
+    </>
+  )
+}
+
+export default UserHomeScreen
